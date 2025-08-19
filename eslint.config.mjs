db1cdfe888a87b427e1db7661cbb31900be6589f
Yash Balotiya <balotiyash@gmail.com>
2025-08-19 15:29:19 +0530

    Initial Commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,11 @@
+/** 
+ * File: eslint.config.mjs
+ * Author: Yash Balotiya
+ * Description: ESLint configuration file
+ * Created on: 19/08/2025
+ * Last Modified: 19/08/2025
+*/
+
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -6,11 +14,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
 export default eslintConfig;
