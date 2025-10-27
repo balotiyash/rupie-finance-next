771a9e65da9d057b31c08d65ac29564205e99432
NehaaBalotia <nehabalotia.clg@gmail.com>
2025-10-27 23:27:07 +0530

    Added Description of files

--- src/components/HomePage/HeroSec.tsx
+++ src/components/HomePage/HeroSec.tsx
@@ -1,5 +1,5 @@
 /** 
- * File: src/components/HomePage/HeroSection.tsx
+ * File: src/components/HomePage/HeroSec.tsx
  * Author: Neha Balotia
  * Description: This file contains the HeroSection component for the homepage.
  * Created on: 26/10/2025
@@ -13,25 +13,26 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 import styles from "./HeroSec.module.css";
 import Typewriter from 'typewriter-effect';
-// import Image from 'next/image';
-// import Person from '../../../public/images/person.png'
 
 const HeroSec = () => {
     return (
         <>
+            {/* Hero Section */}
             <div className={styles.heroSec}>
                 <div className={styles.textContainer}>
                     <h1>Obtain a {" "}
-                    <span className={styles.typewriter}>
-                        <Typewriter
-                            options={{
-                                strings: ['Safe', 'Secure', 'Fast'],
-                                autoStart: true,
-                                loop: true,
-                            }}
-                        />
-                    </span>
-                    Loan </h1>
+                        <span className={styles.typewriter}>
+                            {/* Type Writer for Typing effect. */}
+                            <Typewriter
+                                options={{
+                                    // Array of Words to be displayed in typing one after another.
+                                    strings: ['Safe', 'Secure', 'Fast'],
+                                    autoStart: true,
+                                    loop: true,
+                                }}
+                            />
+                        </span>
+                        Loan </h1>
                 </div>
                 <div className={styles.imageContainer}>
                     <img src="/images/person.png" alt="Person" className={styles.personImg} />
