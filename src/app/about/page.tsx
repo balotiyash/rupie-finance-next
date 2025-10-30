48a3c0c47fa52cba619fdd5b096f42ede199c283
NehaaBalotia <nehabalotia.clg@gmail.com>
2025-10-31 00:34:29 +0530

    Added Get Loan Button

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,40 @@
 import React from 'react'
+import style from './page.module.css'
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
+	return (
+		<main>
+			<div className={style.content}>
+				{/* Left Section of images */}
+				<div className={style.leftSec}>
+					{/* <div>
+						<img src="/images/about-us/loan.jpg" alt="Loan" className={style.leftBox} />
+					</div>
+
+					<div className={style.rightBox}>
+						<div >
+							<img src="/images/about-us/rupee.jpg" alt="Rupees" className={style.box1} />
+						</div>
+						<div className={style.box2}>
+							<img src="/images/about-us/money.jpg" alt="Money" className={style.box2} />
+						</div>
+
+					</div> */}
+
+
+					<img src="/images/about-us/phoneInHand1.png" alt="Loan" />
+
+				</div>
+
+				{/* Right Section of Content */}
+				<div className={style.rightSec}>
+					<h1>ABOUT US</h1> <br />
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+
+			</div>
+		</main>
+	)
 }
 
 export default page
--- src/components/HomePage/HeroSec.tsx
+++ src/components/HomePage/HeroSec.tsx
@@ -14,27 +14,41 @@ import Link from "next/link";
 import styles from "./HeroSec.module.css";
 import Typewriter from 'typewriter-effect';
 
+// Leaves margin at the top and scrolls to typeSection
+const handleScroll = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    const target = document.getElementById("typeSection");
+    if (target) {
+        const offset = 200;
+        const y = target.getBoundingClientRect().top + window.scrollY - offset;
+        window.scrollTo({ top: y, behavior: "smooth" });
+    }
+};
+
 const HeroSec = () => {
     return (
         <>
             {/* Hero Section */}
             <div className={styles.heroSec}>
-                <div className={styles.textContainer}>
-                    <h1>Obtain a {" "}
-                        <span className={styles.typewriter}>
-                            {/* Type Writer for Typing effect. */}
-                            <Typewriter
-                                options={{
-                                    // Array of Words to be displayed in typing one after another.
-                                    strings: ['Safe', 'Secure', 'Fast'],
-                                    // Start Typing whenever the page is opened.
-                                    autoStart: true,
-                                    // Keep typing words in loop
-                                    loop: true,
-                                }}
-                            />
-                        </span>
-                        Loan </h1>
+                <div className={styles.leftSec}>
+                    <div className={styles.textContainer}>
+                        <h1>Obtain a {" "}
+                            <span className={styles.typewriter}>
+                                {/* Type Writer for Typing effect. */}
+                                <Typewriter
+                                    options={{
+                                        // Array of Words to be displayed in typing one after another.
+                                        strings: ['Safe', 'Secure', 'Fast'],
+                                        // Start Typing whenever the page is opened.
+                                        autoStart: true,
+                                        // Keep typing words in loop
+                                        loop: true,
+                                    }}
+                                />
+                            </span>
+                            Loan </h1>
+                    </div>
+                    <Link href="#typeSection" onClick={handleScroll} className={styles.getLoanButton}>Get Loan</Link>
                 </div>
                 {/* Image of a person Holding Phone in Herosection */}
                 <div className={styles.imageContainer}>
